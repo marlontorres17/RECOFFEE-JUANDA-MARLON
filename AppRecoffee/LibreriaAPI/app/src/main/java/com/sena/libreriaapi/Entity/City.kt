2bf61068c6e7b62e4d4e5573008350c578a14f0e
marlontorres17 <marlontorresmedina@gmail.com>
2024-10-09 14:39:41 -0500

    segundo commit

--- AppRecoffee/LibreriaAPI/app/src/main/java/com/sena/libreriaapi/Entity/City.kt
+++
@@ -1,6 +0,0 @@
-package com.sena.libreriaapi.Entity
-
-data class City(
-    val name: String,
-    val id: Int
-)
--- AppRecoffee/LibreriaAPI/app/src/main/java/com/sena/libreriaapi/Entity/Login.kt
+++
@@ -1,6 +0,0 @@
-package com.sena.libreriaapi.Entity
-
-data class Login(
-    val userName: String,
-    val password: String
-)
